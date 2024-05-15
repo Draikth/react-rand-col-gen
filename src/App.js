925aac70a4e4143980618e485b7d087bae261a1a
Marais Devis <maraisdcdevis@gmail.com>
2024-05-15 15:15:51 +0200

    Modify App.js for ran col gen to work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import randomColor from 'randomcolor';
+import { useState } from 'react';
 
 export default function App() {
+  const [col, setCol] = useState(true);
   const color = randomColor();
   console.log(color);
   return (
-    <>
-      <div>
-        <h1>Random Color Generator</h1>
-        <button>Generate</button>
-      </div>
+    <div
+      style={{
+        height: '120px',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}
+    >
+      <h1>Random Color Generator</h1>
+      <button onClick={() => setCol(!col)}>Generate</button>
       <div
         style={{
+          fontWeight: 'bold',
+          fontSize: '100px',
+          marginTop: '8px',
+          width: '700px',
+          display: 'flex',
+          flexDirection: 'column',
           backgroundColor: color,
-          width: '50%',
-          margin: '25%',
+          padding: '10px',
           color: 'white',
-          textShadow:
-            '-0.75px 0 black, 0 0.75px black, 0.75px 0 black, 0 -0.75px black',
+          textShadow: '-1px 0 black, 0 1px black, 1px 0 black, 0 1px black',
+          textAlign: 'center',
+          alignItems: 'center',
         }}
       >
         Generated Color: {color}
       </div>
-    </>
+    </div>
   );
 }
